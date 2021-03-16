2f1f30bef5a77eb8fb36cd3e5a14acbc153d093d
DESKTOP-5VMHJT8\Mohamed Amin Khattab <khattabmohamed174@gmail.com>
2021-03-17 01:20:02 +0200

    Cinemachine switch

--- Assets/___SacrificeWho/Scripts/Gameplay/SwitchCharacter.cs
+++ Assets/___SacrificeWho/Scripts/Gameplay/SwitchCharacter.cs
@@ -4,34 +4,52 @@ using UnityEngine;
 using Cinemachine;
 public class SwitchCharacter : MonoBehaviour
 {
-  
-    [SerializeField] Transform[] targets;
-    [SerializeField] GameObject[] characters;
-    [SerializeField] CinemachineVirtualCamera PersonFollow;
-   
-    Transform oldTarget;
-    GameObject oldCharacter;
-    private void Awake()
+
+    [SerializeField] CinemachineVirtualCamera knightFollow;
+    [SerializeField] GameObject knight;
+    [SerializeField] CinemachineVirtualCamera archerFollow;
+    [SerializeField] GameObject archer;
+    [SerializeField] CinemachineVirtualCamera wizardFollow;
+    [SerializeField] GameObject wizard;
+
+    private void Update()
     {
-        oldTarget = targets[0];
+        if (knight.activeInHierarchy)
+        {
+            knightFollow.m_Priority = 10;
+            archerFollow.m_Priority = 0;
+            wizardFollow.m_Priority = 0;  
+            archer.SetActive(true);
+            wizard.SetActive(true);
+        }
+        if (archer.activeInHierarchy&&!knight.activeInHierarchy)
+        {
+            knightFollow.m_Priority = 0;
+            archerFollow.m_Priority = 10;
+            wizardFollow.m_Priority = 0;
+        }
+        if (wizard.activeInHierarchy&&!archer.activeInHierarchy && !knight.activeInHierarchy)
+        {
+            knightFollow.m_Priority = 0;
+            archerFollow.m_Priority = 0;
+            wizardFollow.m_Priority = 10;
+        }
     }
     public void Switch()
     {
-        for (int i = 0; i < characters.Length; i++)
+      if(knight.activeInHierarchy&&archer.activeInHierarchy&&wizard.activeInHierarchy)
         {
-            if(characters[i].activeInHierarchy)
-            {
-            PersonFollow.Follow = characters[i].transform;
-            PersonFollow.LookAt = targets[i];
-                if(PersonFollow.LookAt==oldTarget/*&&i<=2*/)
-                {
-                    PersonFollow.Follow = characters[i+1].transform;
-                    PersonFollow.LookAt = targets[i+1];
-                }
-                oldTarget = PersonFollow.LookAt;
-            break;
-            }
+            knight.SetActive(false);
         }
-
+        else if (!knight.activeInHierarchy && archer.activeInHierarchy && wizard.activeInHierarchy)
+        {
+            archer.SetActive(false);
+        }
+        else if (!knight.activeInHierarchy && !archer.activeInHierarchy && wizard.activeInHierarchy)
+        {
+            wizard.SetActive(false);
+            knight.SetActive(true);
+            archer.SetActive(true);
+        }
     }
 }
