5b2ef97f22e4e23e065e1f91c04c831a7fe48701
Karim-PC <kgamalali@gmail.com>
2021-03-18 02:23:27 +0200

    added death anim

---
+++ Assets/___SacrificeWho/Scripts/Gameplay/Death.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+public class Death : MonoBehaviour
+{
+    Animator anim;
+    public BoolSO isDead;
+
+    [SerializeField] GameObject target;
+    [SerializeField] CinemachineVirtualCamera cvcam; 
+    private void Start()
+    {
+        isDead.state = false;
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    public void Dead()
+    {
+        isDead.state = true;
+        anim.SetTrigger("Death");
+        target.SetActive(false);
+        cvcam.enabled = false;
+    }
+
+}
